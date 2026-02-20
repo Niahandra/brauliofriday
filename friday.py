645820a764cb599406967e63474c4e09adc38a12
Niahandra <niahandrajeanbaptiste@gmail.com>
2026-02-20 09:13:22 -0400

    b

--- friday.py
+++ friday.py
@@ -2,4 +2,5 @@ import random
 import flet as ft
 
 def main(page: ft.Page):
-    
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
